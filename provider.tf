5bce602957922758292ac45f1e7746981ccf3e86
Dilbag Singh <109151665+dildilbag@users.noreply.github.com>
2022-10-31 09:18:29 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-    subscription_id = "ea9ff72c-0612-4ac9-855f-f7a7809e9354"
-    client_id = "f9b5fdfa-40da-4b39-9b63-3643d8bdca3d"                  
-    tenant_id = "0becb7aa-7321-4bfa-a650-0db08585d35e"
-    client_secret =  "lWk8Q~fwMkdhq0f4FtkcaAO0iSWmG53k23z3Dbow"
+    subscription_id = ""
+    client_id = ""                  
+    tenant_id = ""
+    client_secret =  ""
   features {
     
   }
